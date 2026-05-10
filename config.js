7ae26fad014ab4837019684341a2ac48d9e913ba
TEDDY TECH <kibetwycliffe093@gmail.com>
2026-05-10 13:10:24 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 module.exports = {
   // ===== Bot Identity =====
-  BOT_NAME: "Ultra X",
+  BOT_NAME: "TEDDY-XMD",
   VERSION: "5.0.0",
 
   // ===== Core Settings =====
   PREFIX: ".",
 
   // ===== Owners (without + or spaces) =====
-  OWNER: ["254788460896"],
+  OWNER: ["254799963583"],
 
   // ===== Sticker Settings =====
-  PACK_NAME: "Trashcore Stickers",
-  AUTHOR: "Trashcore",
+  PACK_NAME: "Teddy Stickers",
+  AUTHOR: "Teddy",
 
   // ===== Telegram (TelexWa) =====
   BOT_TOKEN: "", // get from @BotFather on Telegram
